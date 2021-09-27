dc3076c5592a145bd0f244cbb017e12fb3cc2f56
gb4400 <guillaume.beny04@gmail.com>
2021-09-27 17:59:08 +0200

    Update play_sudoku.py

--- src/play_sudoku.py
+++ src/play_sudoku.py
@@ -6,18 +6,18 @@ if len(sys.argv)>1: # si il y a bien deux arguments
     try:
         a = open(inputfile,"r")
         instance = SudokuGrid(a.readline(inputline))
-            for i in range(0,9):
-                for j in range(0,9):
-                    print(instance.grid[i][j]) #affichage de l'etat actuel de la grille
+            
     except:
         print("Impossible de lire le fichier")
 else:
-    input("Veuillez saisir manuellement une grille : ")
+    grille = input("Veuillez saisir manuellement une grille : ")
+    instance = SudokuGrid(grille)
 
-
-    position = int(input("Position de la valeur à ecrire"))
-    valeur = int(input("Valeur à écrire : "))
-    try:
-        #verification de la validité de la saisie
-    except: 
-        print("Veuillez rentrer une saisie valide !")
+while len(instance.get_empty_positions())!=0:
+    for i in range(0,9):    
+        for j in range(0,9):
+            print(instance.grid[i][j]) #affichage de l'etat actuel de la grille
+            position = tuple(input("Position de la valeur à ecrire"))
+            valeur = int(input("Valeur à écrire : "))
+            while position>=0 and position<80 and valeur>=0 and valeur<9:
+            instance.write()
