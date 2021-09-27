e7eb59ec026c8a7c7c6824b2ccf1fdb1b12a285d
Lufou <lucas.damase@laposte.net>
2021-09-27 15:21:49 +0200

    Fixed len bug

--- src/grid.py
+++ src/grid.py
@@ -35,20 +35,20 @@ class SudokuGrid:
 
     def __str__(self):
         res = ""
-        for i in range(self.grid.__len__):
-            for j in range(self.grid[0].__len__):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
                 res += str(self.grid[i][j])
         return res
 
     def get_row(self, i):
         res = []
-        for j in range(self.grid[i].__len__):
+        for j in range(len(self.grid)):
             res.append(self.grid[i][j])
         return res
         
     def get_col(self, j):
         res = []
-        for i in range(self.grid.__len__):
+        for i in range(len(self.grid)):
             res.append(self.grid[i][j])
         return res
 
@@ -67,16 +67,16 @@ class SudokuGrid:
 
     def get_empty_positions(self):
         res = []
-        for i in range(self.grid.__len__):
-            for j in range(self.grid[0].__len__):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
                 if self.grid[i][j] == 0:
                     res.append((i,j))
         return res
 
     def write(self, i, j, v, force=True):
-        if i < 0 or i >= self.grid.__len__:
+        if i < 0 or i >= len(self.grid):
             raise ValueError()
-        elif j < 0 or j >= self.grid[0].__len__:
+        elif j < 0 or j >= len(self.grid[0]):
             raise ValueError()
         elif v < 1 or v > 9:
             raise ValueError()
@@ -86,8 +86,8 @@ class SudokuGrid:
 
     def copy(self):
         new_instance = self.__new__(self.__class__)
-        for i in range(self.grid.__len__):
-            for j in range(self.grid[0].__len__):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
                 new_instance.grid[i][j] = self.grid[i][j]
         return new_instance
         
