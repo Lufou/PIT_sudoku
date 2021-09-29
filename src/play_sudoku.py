2d8eb0970369c05af631097575fd35d6f9af249b
Lufou <lucas.damase@laposte.net>
2021-09-29 17:34:45 +0200

    Finished class play_sudoku.py

--- src/play_sudoku.py
+++ src/play_sudoku.py
@@ -16,12 +16,22 @@ else:
     instance = SudokuGrid(grille)
 
 while len(instance.get_empty_positions())!=0:
-    for i in range(0,9):    
+    for i in range(0,9):
+        print("-------------------")
+        line = "|"
         for j in range(0,9):
-            print(instance.grid[i][j]) #affichage de l'etat actuel de la grille
-    position = input("Position de la valeur à ecrire : ") #saisie utilisateur de la position de la valeur à écrire
+            line += (str(instance.grid[i][j]) + "|")
+        print(line)
+    print("--------------------")
+
+    position = input("Position de la valeur à ecrire sous la forme 0,0 : ")
     sep = position.split(",")
-    #Tuple = tuple(sep)
-    valeur = int(input("Valeur à écrire : ")) #saisie utilisateur de la valeur à écrire
-    while sep[0]>=0 and sep[0]<9 and sep[1]>=0 and sep[1]<9 and valeur>0 and valeur<=9: #verification de la validité de la saisie
-        instance.write(sep[0],sep[1],valeur) #inscription dans la grille de la valeur donnée à la position donnée
+    while not sep[0].isnumeric() or not sep[1].isnumeric() or int(sep[0]) < 0 or int(sep[0]) >= 9 or int(sep[1]) < 0 or int(sep[1]) >= 9:
+        position = input("Position de la valeur à ecrire sous la forme 0,0 : ")
+        sep = position.split(",")
+
+    valeur = input("Valeur à écrire (de 1 à 9) : ")
+    while not valeur.isnumeric() or int(valeur) <= 0 or int(valeur) > 9 or instance.get_row(int(sep[0])).__contains__(int(valeur)) or instance.get_col(int(sep[1])).__contains__(int(valeur)) or instance.get_region(int(sep[0])//3,int(sep[1])//3).__contains__(int(valeur)):
+        valeur = input("Vous n'êtes pas autorisé à mettre cette valeur, entrez une nouvelle valeur (de 1 à 9) : ")
+
+    instance.write(int(sep[0]),int(sep[1]),int(valeur))
